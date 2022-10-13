43faa9ce8934fc7bcd8c14836a429e82e21bbfdf
sagar <davidvidyasagar@outlook.com>
2022-10-13 14:21:54 +0100

    changes

--- Niharika/reading&writing_files.py
+++ Niharika/reading&writing_files.py
@@ -4,3 +4,67 @@ print(f.mode)
 print(f.name)
 
 f.close()
+
+
+with open('test.txt','r') as f:
+    print(f.name)
+
+when we use with(), it will close the file explicitly when the block is ended.
+
+
+Using with is called as ******Context Manager ********
+
+
+f.readline()
+f.read()
+f.read(100) - reads first 100 characters.
+
+a = f.read(100)
+
+while len(a) > 0:
+    print(a, end='*') 
+
+# f.seek(0) - to assign the position 
+
+# This creates a new file and writes 'Firstline' to that file.
+
+with open('test2.txt', 'w') as f:
+    f.write('Firstline')    
+
+# To create a copy of file.
+
+
+with open('test1.py', 'r') as rf:
+    with open('test1_copy.py', 'w') as wf:
+        for line in rf:
+            wf.write(line)
+
+for binary files, use rb and wb
+
+# for csv files import csv module and use csv.reader()
+
+
+import csv
+
+with open('a.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for i in csv_reader:
+        print(i)
+        print(i[2]) - only third value in a file.
+
+    
+with open('a.csv', 'r') as csv_file:
+    with open('b.csv', 'w') as copy_file:
+
+    csv_writer = csv.writer(copy_file, delimiter = '-')
+    for i in csv_reader:
+        csv_writer.writerow(i)
+      
+# Dictreader and Dictwriter to get values as key value pair
+
+
+
+
+
+    
+
--- PythonOOPs/Class_and_Instances.py
+++ PythonOOPs/Class_and_Instances.py
@@ -1,76 +1,136 @@
-# Python Object Oriented Programming
-# Method is a function associated with a class
-# Class is a blue print for creating instances
-# Instance of a class - 
+# # Python Object Oriented Programming
+# # Method is a function associated with a class
+# # Class is a blue print for creating instances
+# # Instance of a class - 
 
-class Employee:
-    num_of_emp = 0
-    raise_amount =  1.04
-    def __init__(self, first, last, pay):       # This __init__ is initialise or constructor
-        self.first = first
-        self.last = last
-        self.email = self.first + '.' + self.last + '@abc.com'
-        self.pay = pay
+# class Employee:
+#     num_of_emp = 0
+#     raise_amount =  1.04
+#     def __init__(self, first, last, pay):       # This __init__ is initialise or constructor
+#         self.first = first
+#         self.last = last
+#         self.email = self.first + '.' + self.last + '@abc.com'
+#         self.pay = pay
 
-        Employee.num_of_emp += 1
+#         Employee.num_of_emp += 1
 
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
     
-    def apply_raise(self):
-        # self.pay = int(self.pay * Employee.raise_amount)
-        self.pay = int(self.pay * self.raise_amount)    # This is good and recommended
+#     def apply_raise(self):
+#         # self.pay = int(self.pay * Employee.raise_amount)
+#         self.pay = int(self.pay * self.raise_amount)    # This is good and recommended
 
-# emp_1 and emp_2 are the instance of class Employee
-emp_1 = Employee('Sagar', 'Gadda', 5000)
-emp_2 = Employee('Niharika', 'Nichenametla', 6000)
+# # emp_1 and emp_2 are the instance of class Employee
+# emp_1 = Employee('Sagar', 'Gadda', 5000)
+# emp_2 = Employee('Niharika', 'Nichenametla', 6000)
 
-# Instance variable contains data that is unique to each instance, we can manually create them
+# # Instance variable contains data that is unique to each instance, we can manually create them
 
-# print(emp_1)
-# print(emp_2)
+# # print(emp_1)
+# # print(emp_2)
 
-# Manually defining the instance variables - This is not a recommended approach, so I will comment them
+# # Manually defining the instance variables - This is not a recommended approach, so I will comment them
 
-# emp_1.first = 'Sagar'
-# emp_1.last = 'Gadda'
-# emp_1.email = 'Sagar.Gadda@abc.com'
-# emp_1.pay = 5000
+# # emp_1.first = 'Sagar'
+# # emp_1.last = 'Gadda'
+# # emp_1.email = 'Sagar.Gadda@abc.com'
+# # emp_1.pay = 5000
 
-# emp_2.first = 'Niharika'
-# emp_2.last = 'Nichenametla'
-# emp_2.email = 'Niharika.Nichenametla@abc.com'
-# emp_2.pay = 6000
+# # emp_2.first = 'Niharika'
+# # emp_2.last = 'Nichenametla'
+# # emp_2.email = 'Niharika.Nichenametla@abc.com'
+# # emp_2.pay = 6000
 
-print(emp_1.email)
-print(emp_2.email)
+# print(emp_1.email)
+# print(emp_2.email)
 
-print(emp_1.fullname())
-print(emp_2.fullname())
-print(Employee.fullname(emp_1))
-print(Employee.fullname(emp_2))
+# print(emp_1.fullname())
+# print(emp_2.fullname())
+# print(Employee.fullname(emp_1))
+# print(Employee.fullname(emp_2))
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
 
-# We can access class variables from both class itself and instance as well
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+# # We can access class variables from both class itself and instance as well
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
 
-# print(Employee.__dict__)
-# print(emp_1.__dict__)
+# # print(Employee.__dict__)
+# # print(emp_1.__dict__)
 
-Employee.raise_amount = 1.05
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+# Employee.raise_amount = 1.05
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
 
-print('*'*50)
-emp_1.raise_amount = 1.06
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
-print('*'*50)
-print(Employee.num_of_emp)
+# print('*'*50)
+# emp_1.raise_amount = 1.06
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+# print('*'*50)
+# print(Employee.num_of_emp)
+
+# niharika
+
+# class Employee:
+#     pass
+# emp1 = Employee()
+# emp2 = Employee()
+
+# print(emp1)
+# print(emp2)
+
+# emp1.first = "niharika"
+
+# print(emp1.first)
+
+
+# class employee2:
+#     def __init__(self,first,last):
+#         self.first = first
+#         self.last = last
+# emp1 = employee2('niha', 'honey')
+# emp2 = employee2('neera', 'sitara')      
+# print(emp1)
+# print(emp2)  
+
+# class employee3:
+
+#     def __init__(self,first,last):
+#         self.fname = first
+#         self.lname = last
+#     def fullname(self):
+#         print(f'{self.fname} {self.lname}')
+
+
+# emp1 = employee3('sagar', 'gadda')
+# emp2 = employee3('neera', 'sitara')
+# print(emp1.fname)
+# emp1.fullname()
+
+# employee3.fullname(emp2)
+
+# class emp:
+#     hike = 50
+#     num_emp = 0
+#     def __init__(self,name,pay):
+#         self.name = name
+#         self.pay = pay
+#         emp.num_emp = emp.num_emp + 1
+#     def new_pay(self):
+#         self.pay = self.pay + (self.hike * self.pay) / 100
+#         print(self.pay)
+
+# e1 = emp('vinnie',100)
+# e2 = emp('disney',200)
+
+# emp.new_pay(e2)
+# e1.hike = 90
+# e1.new_pay()
+# print(emp.num_emp)
+
